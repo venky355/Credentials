da962cc25ec6de6f494b20ad4fc18f3b866bc614
venkatesh <kasivenkatesh12@gmail.com>
2024-04-01 15:18:17 +0530

    Credentials

--- Credentialsapp/forms.py
+++ Credentialsapp/forms.py
@@ -4,10 +4,10 @@ from Credentialsapp.models import User
 class UserRegistrationForm(forms.ModelForm):
     class Meta:
         model = User  
-        fields = ['username', 'password', 'email']  
+        fields = ['username', 'password', 'email','role']  
         
 
 class DealerRegistrationForm(forms.ModelForm):
     class Meta:
         model = User  
-        fields = ['username', 'password', 'email', 'dealer_field']  
+        fields = ['username', 'password', 'email','role', 'dealer_field']  
